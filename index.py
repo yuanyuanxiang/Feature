bf3578121585f82937e89deb8cdb69f533119f87
yuanyuanxiang <962914132@qq.com>
2019-10-22 21:06:58 +0800

    style:减少和优化代码

--- index.py
+++ index.py
@@ -4,54 +4,89 @@ import h5py
 import numpy as np
 import argparse
 
-from extract_cnn_vgg16_keras import VGGNet
+from numpy import linalg as LA
+
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-database", required = True,
-    help = "Path to database which contains images to be indexed")
-ap.add_argument("-index", required = True,
-    help = "Name of index file")
-args = vars(ap.parse_args())
+class VGGNet:
+    def __init__(self):
+        # weights: 'imagenet'
+        # pooling: 'max' or 'avg'
+        # input_shape: (width, height, 3), width and height should >= 48
+        self.input_shape = (224, 224, 3)
+        self.weight = 'imagenet'
+        self.pooling = 'max'
+        self.model = VGG16(weights=self.weight, input_shape=(
+            self.input_shape[0], self.input_shape[1], self.input_shape[2]),
+            pooling=self.pooling, include_top=False)
+        self.model.predict(np.zeros((1, 224, 224, 3)))
+
+    '''
+    Use vgg16 model to extract features
+    Output normalized feature vector
+    '''
+
+    def features(self, img_path):
+        img = image.load_img(img_path, target_size=(
+            self.input_shape[0], self.input_shape[1]))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img)
+        feat = self.model.predict(img)
+        norm_feat = feat[0]/LA.norm(feat[0])
+        return norm_feat
+
+    def featureAll(self, img_dir):
+        img_list = get_imlist(img_dir)
+        feats = []
+        names = []
+        for i, img_path in enumerate(img_list):
+            norm_feat = self.features(img_path)
+            img_name = os.path.split(img_path)[1]
+            feats.append(norm_feat)
+            names.append(img_name.encode())
+            print("extracting feature from image No. %d / %d" %((i+1), len(img_list)))
+
+        feats = np.array(feats)
+        return names, feats
+
 
 '''
  Returns a list of filenames for all jpg images in a directory. 
 '''
+
+
 def get_imlist(path):
-    return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')]
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')]
+
 
 '''
  Extract features and index the images
+ # 对database文件夹内图片进行特征提取，建立索引文件featureCNN.h5
+ # python index.py -database database -index featureCNN.h5
 '''
 if __name__ == "__main__":
 
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-database", required=False,
+                    help="Path to database which contains images to be indexed",
+                    default="D:\\照片\\2019【趣探索】中秋节")
+    ap.add_argument("-index", required=False,
+                    help="Name of index file", default="featureCNN.h5")
+    args = vars(ap.parse_args())
+    # directory of images
     db = args["database"]
-    img_list = get_imlist(db)
-    
-    print ("--------------------------------------------------")
-    print ("         feature extraction starts")
-    print ("--------------------------------------------------")
-    
-    feats = []
-    names = []
-
-    model = VGGNet()
-    for i, img_path in enumerate(img_list):
-        norm_feat = model.extract_feat(img_path)
-        img_name = os.path.split(img_path)[1]
-        feats.append(norm_feat)
-        names.append(img_name.encode())
-        print ("extracting feature from image No. %d , %d images in total" %((i+1), len(img_list)))
-
-    feats = np.array(feats)
     # directory for storing extracted features
     output = args["index"]
-    
-    print ("--------------------------------------------------")
-    print ("      writing feature extraction results ...")
-    print ("--------------------------------------------------")
-    
+
+    model = VGGNet()
+    names, feats = model.featureAll(db)
+
     h5f = h5py.File(output, 'w')
-    h5f.create_dataset('dataset_1', data = feats)
-    h5f.create_dataset('dataset_2', data = names)
+    h5f.create_dataset('dataset_1', data=feats)
+    h5f.create_dataset('dataset_2', data=names)
     h5f.close()
+    print("Finish")
